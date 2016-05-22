d1dd3639536921bba13910caead591cbb3b7c5f7
BastienLozach <bastien.lozach.sio@gmail.com>
2016-05-22 21:35:59 +0200

    C2

--- src/GsbBundle/Controller/ConnexionController.php
+++ src/GsbBundle/Controller/ConnexionController.php
@@ -12,7 +12,31 @@ class ConnexionController extends Controller
     }
     
     public function connexionAction(){
-        return $this->render('GsbBundle:Connexion:index.html.twig', array('name' => "test"));
+        $name = "test";
+        
+        $form = $this->createFormBuilder()
+                ->add('login', 'text')
+                ->add('motDePasse', 'password')
+                ->add('valider', 'submit')
+                ->add('annuler', 'reset')
+                ->getForm();
+        
+        $request = $this->container->get('request');
+        $form->handleRequest($request);
+        
+        if($form->isValid()){
+            $data = $form->getData();
+            // Condition pour la connexion
+            // Reste a allez vérifier dans la BDD
+            
+            if($data["login"] == "Moi" && $data["motDePasse"] == "azerty"){
+                return $this->render('GsbBundle:Template:Template.html.twig', array(
+                    'page' => "Accueil",
+                    'name' => $data["login"]));
+            }
+        }
+        
+        return $this->render('GsbBundle:Connexion:index.html.twig', array('name' => $name, 'form' => $form->createView()));
         
     }
 }
